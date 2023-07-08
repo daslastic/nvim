d4da0b4ce35fe1d3b90cd803084b53acafa3f403
daslastic <daslastic@gmail.com>
2023-07-08 16:01:38 -0400

    autopacker

--- lua/daslastic/packer.lua
+++ lua/daslastic/packer.lua
@@ -1,5 +1,18 @@
 vim.cmd([[packadd packer.nvim]])
 
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 	use("nvim-lua/plenary.nvim")
@@ -96,4 +109,8 @@ return require("packer").startup(function(use)
 	-- dap
 	use("mfussenegger/nvim-dap")
 	use("rcarriga/nvim-dap-ui")
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
