fd11e2b26911ab309afc18b5eb02120b428ca77f
daslastic <daslastic@gmail.com>
2023-08-23 01:20:13 -0400

    feet: tmux resize

--- lua/daslastic/after/tmux.lua
+++ lua/daslastic/after/tmux.lua
@@ -3,4 +3,16 @@ if not tmux then
   return
 end
 
-tmux.setup()
+tmux.setup({
+  resize = {
+    enable_default_keybindings = false,
+    resize_step_x = 2,
+    resize_step_y = 2,
+  }
+})
+
+
+vim.keymap.set("n", "<C-Up>", '<cmd>lua require("tmux").resize_top()<cr>')
+vim.keymap.set("n", "<C-Down>", '<cmd>lua require("tmux").resize_bottom()<cr>')
+vim.keymap.set("n", "<C-Left>", '<cmd>lua require("tmux").resize_left()<cr>')
+vim.keymap.set("n", "<C-Right>", '<cmd>lua require("tmux").resize_right()<cr>')
